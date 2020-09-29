4642b877dc015b94291a855e6b09caa97794c5db
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-29 18:23:10 +0000

    added k(c)lasses

--- dnd_5e/lib/dnd_5e/cli.rb
+++ dnd_5e/lib/dnd_5e/cli.rb
@@ -3,5 +3,7 @@ class Dnd5e::CLI
     puts ""
     puts "Welcome to the 5th Edtion Dungen Manuel"
     puts ""
+    puts "Select the # of the class you want to learn about"
+    list
   end
 end
---
+++ dnd_5e/lib/dnd_5e/klass.rb
@@ -0,0 +1,30 @@
+class Klass
+  
+  def list 
+    puts ""
+    puts "1. Barbarian" # {i}. {name}
+    puts"-----------"
+    puts "2. Bard"
+    puts"-----------"
+    puts "3. Cleric"
+    puts"-----------"
+    puts "4. Druid"
+    puts"-----------"
+    puts "5. Fighter"
+    puts"-----------"
+    puts "6. Monk"
+    puts"-----------"
+    puts "7. Paladin"
+    puts"-----------"
+    puts "8. Ranger"
+    puts"-----------"
+    puts "9. Rouge"
+    puts"-----------"
+    puts "10. Sorcerer"
+    puts"-----------"
+    puts "11. Warlock"
+    puts"-----------"
+    puts "12. Wizard"
+    puts""
+  end
+end
